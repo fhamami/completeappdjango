8c0082a3f5847c67ff1ebb88f86549cfb61d0dd3
fhamami <fuadgoinet@gmail.com>
2018-03-27 23:23:25 +0700

    deploy

--- mainapp/settings/base.py
+++ mainapp/settings/base.py
@@ -18,7 +18,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
+    # 'rest_framework',
     'debug_toolbar',
     'foreignkey',
     'blog',
