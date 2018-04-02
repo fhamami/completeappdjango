3ae99b82fbb33db6d83a59036a0ab040f9e5cb6a
fhamami <fuadgoinet@gmail.com>
2018-04-02 14:42:33 +0700

    deploy

--- mainapp/settings/staging.py
+++ mainapp/settings/staging.py
@@ -73,14 +73,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mainapp.wsgi.application'
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
-DATABASES = {'default': dj_database_url.config()}
+# DATABASES = {'default': dj_database_url.config()}
 
 
 AUTH_PASSWORD_VALIDATORS = [
