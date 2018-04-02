1992ef36a826ef1cf16d1a6534af7bf4c52a3169
fhamami <fuadgoinet@gmail.com>
2018-04-02 23:01:08 +0700

    deploy

--- mainapp/settings/production.py
+++ mainapp/settings/production.py
@@ -68,7 +68,7 @@ WSGI_APPLICATION = 'mainapp.wsgi.application'
 DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 AUTH_PASSWORD_VALIDATORS = [
     {
