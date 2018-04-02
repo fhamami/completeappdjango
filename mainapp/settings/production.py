4f7e00072df6e17aab45af4383447eb8ab6de2d3
fhamami <fuadgoinet@gmail.com>
2018-04-02 23:22:29 +0700

    deploy

--- mainapp/settings/production.py
+++ mainapp/settings/production.py
@@ -1,7 +1,7 @@
 import os
 import django_heroku
-import psycopg2
-import dj_database_url
+# import psycopg2
+# import dj_database_url
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(
@@ -60,14 +60,14 @@ WSGI_APPLICATION = 'mainapp.wsgi.application'
 # DATABASE_URL= postgres://brxugwupkjrrfj:3abcea69b15b723f055c886af13846175a3dda66b7716285b6c761dbd7b1d4b1@ec2-23-21-121-220.compute-1.amazonaws.com:5432/d6e1snktv37p8k
 
 DATABASES = {
-    'default': dj_database_url.config()
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
+    # 'default': dj_database_url.config()
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'datab.sqlite3'),
+    }
 }
-DATABASE_URL = os.environ['DATABASE_URL']
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+# DATABASE_URL = os.environ['DATABASE_URL']
+# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
