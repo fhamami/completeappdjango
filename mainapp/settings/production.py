06cd470476d09cc6823102040abfa351c32f5e4f
fhamami <fuadgoinet@gmail.com>
2018-04-02 22:58:08 +0700

    deploy

--- mainapp/settings/production.py
+++ mainapp/settings/production.py
@@ -65,9 +65,10 @@ WSGI_APPLICATION = 'mainapp.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-# DATABASE_URL = os.environ['DATABASE_URL']
-# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 AUTH_PASSWORD_VALIDATORS = [
     {
