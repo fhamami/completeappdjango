b1c6f5767358e9d5abdf040e0c8b3a53735e4b3c
fhamami <fuadgoinet@gmail.com>
2018-04-09 08:27:54 +0700

    nav and form

--- homepage/urls.py
+++ homepage/urls.py
@@ -4,7 +4,7 @@ from .views import HomeView, Linklist, change_friends
 app_name = 'homepage'
 
 urlpatterns = [
-    path('home/', HomeView, name='home'),
+    path('userposts/', HomeView, name='userposts'),
     path('', Linklist, name='linklist_view'),
     path('connect/(<operation>.+)/(<int:pk>\d+)/', change_friends,
          name='change_friends')
